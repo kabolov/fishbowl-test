f3de6d4a646debab7a214eecebf08f9432dcb68c
Alexander <alexander.kabolov@gmail.com>
2021-07-02 19:56:11 +0200

    remooving unnecessary comments

--- lib/typings.ts
+++ lib/typings.ts
@@ -8,20 +8,9 @@ export enum OPERATORS {
   '$ne' = '!=',
   '$in' = 'IN',
 }
-// export enum OPERATORS {
-//   OR = '$or',
-//   AND = '$and',
-//   LESS_THAN = '$lt',
-//   LESS_THAN_OR_EQUAL = '$lte',
-//   GREATER_THAN = '$gt',
-//   GREATER_THAN_OR_EQUAL = '$gte',
-//   NOT_EQUAL = '$ne',
-//   IN = '$in',
-// }
 
 export interface IParsedQuery {
   collectionName: string;
   query: { [key: string]: { [objKey: string]: any[] } };
-  // query: Record<OPERATORS | string, string | undefined>;
   projection: Record<string, any | string | undefined>;
 }
